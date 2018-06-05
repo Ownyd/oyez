e96f383a4deec6d20b1f244d8ddca25858d1d779
Thibault LUX <tlux@e1r12p6.42.fr>
2018-06-05 19:27:05 +0200

    First commit / RESTful API

---
+++ exo-crud/server.js
@@ -0,0 +1,62 @@
+/* Requirements */
+
+var express = require('express');
+var app = express();
+var mongoClient = require("mongodb").MongoClient;
+var mongoURL = "mongodb://localhost/oyez";
+
+/* DB Creation */
+
+mongoClient.connect(mongoURL, function(err, db) {
+	if (err) throw err;
+	console.log("Database created!");
+	db.close();
+});
+
+/* Collection `strings` creation */
+
+mongoClient.connect(mongoURL, function(err, db) {
+	if (err) throw err;
+	var dbo = db.db("oyez");
+	dbo.createCollection("strings", function(err, res) {
+		if (err) throw err;
+		console.log("Collection created!");
+		db.close();
+	});
+});
+
+/* API */ //TODO : Rajouter timeout sur les api
+app.get("/api/create", function(req, res){
+	mongoClient.connect(mongoURL, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db("oyez");
+		var myobj = { userId: req.query.userID, string: req.query.string };
+		dbo.collection("strings").insertOne(myobj, function(err, res) {
+			if (err) throw err;
+			console.log("Added the record "+req.query.string+"for user N."+req.query.userID+".');
+			db.close();
+		});
+	});
+});
+
+app.get("/api/delete", function(req, res){
+	mongoClient.connect(mongoURL, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db("oyez");
+		var myobj = { userId: req.query.userID, string: req.query.string };
+		dbo.collection("strings").removeOne(myobj, function(err, res) {
+			if (err) throw err;
+			console.log("Removed the record "+req.query.string+"for user N."+req.query.userID+".");
+			db.close();
+		});
+	});
+});
+
+/* 404 */
+
+app.use(function(req, res, next){
+	res.setHeader('Content-Type', 'text/plain');
+	res.status(404).send('Page introuvable !');
+})
+
+.listen(8080);
